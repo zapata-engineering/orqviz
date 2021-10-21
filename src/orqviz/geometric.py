12826cc37eeed9ca1a6d8372ff1671e4319f1755
Manuel S. Rudolph <63448124+MSRudolph@users.noreply.github.com>
2021-10-21 15:40:32 +0200

    adding function to distribute along path (#6)
    * adding function to distribute along path
    
    * Fix in relative_periodic_trajectory_wrap.
    
    * Added tests to geometric.
    
    Co-authored-by: mstechly <michal.stechly@gmail.com>

--- src/orqviz/geometric.py
+++ src/orqviz/geometric.py
@@ -1,6 +1,7 @@
 from typing import Optional, Tuple
 
 import numpy as np
+from scipy.interpolate import interp1d
 
 from .aliases import ArrayOfParameterVectors, ParameterVector
 
@@ -53,8 +54,12 @@ def relative_periodic_trajectory_wrap(
     period: float = 2 * np.pi,
 ) -> ArrayOfParameterVectors:
     """Function that returns a wrapped 'copy' of a parameter trajectory such that
-        the distance between it and the reference point is minimal inside
-        the specified period.
+        the distance between the final point of the trajectory and the reference point
+        is minimal inside the specified period.
+        The rest of the trajectory is being transformed in the same manner.
+        NOTE:
+            It only works as intended if the period is larger than the distance
+            between the consecutive points in the trajectory.
 
     Args:
         reference_point: Reference point for periodic wrapping of the trajectory.
@@ -64,13 +69,17 @@ def relative_periodic_trajectory_wrap(
         period: Periodicity of each parameter in each point of the trajectory.
             Defaults to 2*np.pi.
     """
-    wrapped_trajectory = np.copy(trajectory)
+    if not np.all(np.linalg.norm(np.diff(trajectory, axis=0), axis=1) < period):
+        raise ValueError(
+            "Distances between consecutive points must be smaller than period."
+        )
+    wrapped_trajectory = np.copy(trajectory).astype(float)
     wrapped_trajectory[-1] = relative_periodic_wrap(
         reference_point, trajectory[-1], period=period
     )
     for ii in range(2, len(wrapped_trajectory) + 1):
         wrapped_trajectory[-ii] = relative_periodic_wrap(
-            wrapped_trajectory[-1], trajectory[-ii], period=period
+            wrapped_trajectory[-ii + 1], trajectory[-ii], period=period
         )
 
     return wrapped_trajectory
@@ -114,3 +123,28 @@ def direction_linspace(
         origin + endpoints[1] * direction,
         num=n_points,
     )
+
+
+def uniformly_distribute_trajectory(
+    parameter_trajectory: ArrayOfParameterVectors,
+    n_points: int,
+) -> ArrayOfParameterVectors:
+    """Function to distribute points uniformly (in euclidean distance) along a path
+    that is given by a parameter trajectory, i.e. an array of parameter vectors.
+    Returns an array of parameter vectors where the first and last entries match
+    those of the passed parameter trajectory and all entries are equally distant
+    in euclidean distance.
+    """
+    trajectory_weights = np.linalg.norm(np.diff(parameter_trajectory, axis=0), axis=1)
+    trajectory_weights /= np.sum(trajectory_weights)
+    cum_weights = np.cumsum(trajectory_weights)
+    cum_weights = np.insert(cum_weights, 0, 0)
+    cum_weights[-1] = 1  # for stability insert an integer 1
+
+    weight_interpolator = interp1d(
+        cum_weights,
+        parameter_trajectory,
+        axis=0,
+    )
+    eval_points = np.linspace(0, 1, num=n_points)
+    return weight_interpolator(eval_points)
