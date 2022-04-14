Fix x and y scale/normalization issues with DCT

src/orqviz/dct.py
@@ -24,6 +24,7 @@ def scan_2D_dct(
     dct_resolution_x: int = None,
     dct_resolution_y: int = None,
 ):
+    # should x and y even be diff arguments? like should we support dct on non-squares?
     scan2D_result = perform_2D_scan(
         origin,
         loss_function,
@@ -34,49 +35,49 @@ def scan_2D_dct(
         end_points_x=end_points_x,
         end_points_y=end_points_y,
     )
-    intermediate = []
-    for row in scan2D_result.values:
-        intermediate.append(dct(row))
-    intermediate = np.array(intermediate)
-    intermediate = intermediate.T
-    result = []
-    for row in intermediate:
-        result.append(dct(row))
-    result = np.array(result).T
+    result = dct_2d(scan2D_result.values)
     if n_steps_y is None:
         n_steps_y = n_steps_x
     if dct_resolution_x is None:
         dct_resolution_x = n_steps_x
     if dct_resolution_y is None:
         dct_resolution_y = dct_resolution_x
+    frequency_normalization_factor = (end_points_x[1] - end_points_x[0]) / np.pi
+    normalized_x_res = dct_resolution_x * int(frequency_normalization_factor)
+    normalized_y_res = dct_resolution_y * int(frequency_normalization_factor)
     grid = np.array(
-        [[[x, y] for x in range(dct_resolution_x)] for y in range(dct_resolution_y)]
+        [
+            [
+                [x / frequency_normalization_factor, y / frequency_normalization_factor]
+                for x in range(normalized_x_res)
+            ]
+            for y in range(normalized_y_res)
+        ]
     )
-    # truncate result according with resolution.
-    result = result[0:dct_resolution_x, 0:dct_resolution_y]
+    # Truncate result according with resolution.
+    result = result[0:normalized_x_res, 0:normalized_y_res]
     return Scan2DResult(grid, direction_x, direction_y, result)
 
 
+def dct_2d(x: np.ndarray) -> np.ndarray:
+    assert len(x.shape) == 2
+    intermediate = []
+    for row in x:
+        intermediate.append(dct(row))
+    intermediate = np.array(intermediate)
+    intermediate = intermediate.T
+    result = []
+    for row in intermediate:
+        result.append(dct(row))
+    return np.array(result).T
+
+
 def dct(x: np.ndarray) -> np.ndarray:
-    """DCT-II"""
+    """DCT-II 1D transformation. It's normalized."""
     assert len(x.shape) == 1
     N = x.size
     X = np.zeros(N)
     for k in range(N):
         X[k] = np.dot(x, np.cos((np.arange(N) + 0.5) * np.pi / N * k))
-    return X
-
-
-def dct_1(x: np.ndarray) -> np.ndarray:
-    """DCT-I"""
-    assert len(x.shape) == 1
-    N = x.size
-    X = np.zeros(N)
-    for k in range(N):
-        term_1 = 0.5 * (x[0] + (-1) ** k * x[N - 1])
-        # term_2 = 0
-        # for n in range(1, N - 1):
-        #     term_2 += x[n] * np.cos((np.pi / (N - 1)) * n * k)
-        term_2 = np.dot(x[1:-1], np.cos((np.pi / (N - 1)) * (np.arange(N - 2) + 1) * k))
-        X[k] = term_1 + term_2
-    return X
+    # Normalize result by factor of X * 2 / N
+    return X * 2 / N