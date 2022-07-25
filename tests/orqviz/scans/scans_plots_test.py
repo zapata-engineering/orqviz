e6cdee9928992f9ecfce79a867d73b44d19a5ab6
Laura Gao <lauragao86868@gmail.com>
2022-07-25 17:55:18 -0400

    Fix isort

--- tests/orqviz/scans/scans_plots_test.py
+++ tests/orqviz/scans/scans_plots_test.py
@@ -1,13 +1,8 @@
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 from orqviz.scans import perform_1D_scan, perform_2D_interpolation
-from orqviz.scans.plots import (
-    plot_1D_scan_result,
-    plot_2D_interpolation_result,
-)
+from orqviz.scans.plots import plot_1D_scan_result, plot_2D_interpolation_result
 
 
 def SUM_OF_SINES(params):
